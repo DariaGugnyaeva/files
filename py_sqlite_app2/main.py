56680d5f9beec604667f58fc17003a353af3af08
Bee <113686174+DariaGugnyaeva@users.noreply.github.com>
2025-03-06 12:20:49 +0300

    Update main.py

--- py_sqlite_app2/main.py
+++ py_sqlite_app2/main.py
@@ -74,25 +74,69 @@ def main():
                             f"title: {task.title}, status: {task.status}, project_id: {task.project_id}"
                         )
                     
-                case "find" | "search" | "f" | "s":
-                    pattern = " ".join(args[1:])
-                    tasks = session.query(Task).filter(or_(Task.name.contains(pattern), Task.description.contains(pattern))).all()
-                    print(f"Found {len(tasks)} task(s)")
-                    for task in tasks:
-                        print(
-                            f"id: {task.id}, name: {task.name}, description: '{task.description}', completed: {task.completed}"
-                        )
-
-                case "complete" | "co" | "c":
-                    id = int(args[1])
-                    task = session.query(Task).filter_by(id=id).one()
-                    task.completed = True
+                case "update-user" | "обновление пользователя":
+                    pattern = args[1]
+                    user = session.query(User).filter_by(id=id).one()
+                    if '@' in pattern:
+                        user.email = pattern
+                    else:
+                        user.username = pattern
                     session.commit()
 
-                case "delete" | "d":
-                    id = int(args[1])
-                    task = Task.get(id)
-                    session.delete(task)
+                case "update-prof" | "обновление профиля":
+                    pattern = ' '.join(args[1:])
+                    prof = session.query(Profile).filter_by(id=id).one()
+                    if all(character.isdigit() for character in pattern):
+                        user.phone = pattern
+                    else:
+                        prof.bio = pattern
+                    session.commit()
+        
+                case "update-proj" | "проект выполнен" | "проект изменился" | "проект не выполнен":
+                    pattern = ' '.join(args[1:])
+                    proj = session.query(Project).filter_by(id=id).one()
+                    if ' ' in pattern:
+                        proj.description = pattern
+                    else:
+                        proj.title = pattern  # напоминаю, через _
+                    session.commit()
+                    
+    
+                case "update-task" | "задача выполнена" | "задача изменилась" | "переделать" | "задача не выполнена":
+                    pattern = ' '.join(args[1:])
+                    task = session.query(Task).filter_by(id=id).one()
+                    if pattern in ['True', True, 'False', False]:
+                        task.status = bool(pattern)
+                    # хорошую задачу 1 словом назовёшь, но не опишешь...
+                    elif ' ' not in pattern:
+                        task.title = pattern
+                    else:
+                        task.desription = pattern
+                    session.commit()
+
+                case "delete-user" | "d-u" | "удалить пользователя и его профиль":
+                    id = args[1]
+                    user_id = User.get(id)
+                    user_to_delete = session.query(User).delete(user_id)
+                    # в Profile есть столбец user_id. Что делать?
+                    profile_to_delete = session.query(Profile).delete(user_id)
+                    session.delete(user_to_delete)
+                    session.delete(profile_to_delete)
+                    session.commit()
+
+                case "delete-proj" | "d-proj" | "удалить проект":
+                    # title and description
+                    id = args[1]
+                    proj_id = Project.get(id)
+                    proj_to_delete = session.query(Project).delete(proj_id)
+                    session.delete(proj_to_delete)
+                    session.commit()
+
+                case "delete-task" | "d-t" | "удалить задачу":
+                    id = args[1]
+                    task_id = Task.get(id)
+                    task_to_delete = session.query(Task).delete(task_id)
+                    session.delete(task_to_delete)
                     session.commit()
 
                 case "quit" | "q" | "exit":
