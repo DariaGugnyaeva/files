f67520522523b74c7e4c9814172d7f68abe995cd
Bee <113686174+DariaGugnyaeva@users.noreply.github.com>
2025-03-06 10:18:17 +0300

    Update models.py

--- py_sqlite_app2/db/models.py
+++ py_sqlite_app2/db/models.py
@@ -4,11 +4,30 @@ from db import engine
 
 Base = declarative_base()
 
-class Task(Base):
-    __tablename__ = "tasks"
+class Books(Base):
+    __tablename__ = "Books"
     id = Column(Integer, primary_key=True)
-    name = Column(String(255), nullable=False, unique=True)
-    description = Column(String(255), nullable=False)
-    completed = Column(Boolean, default=False)
+    name = Column(String(40), nullable=False)
+    author = Column(String(40), nullable=False)
+    year = Column(Integer, nullable=False, unique=False, default=2025)
+    edition = Column(Integer, nullable=False)
+    num_bookcase = Column(Integer, nullable=False)
+    num_shell = Column(Integer, nullable=False)
+
+class Visitors(Base):
+    # имя, фамилия, отчество, номер читательского билета, адрес
+    __tablename__ = "Visitors"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30), nullable=False)
+    surname = Column(String(30), nullable=False)
+    patr_name = Column(String(30), nullable=False, default="-")
+    library_card = Column(Integer, unique=True)
+    address = Column(String(50), nullable=False)
+
+class Readers(Base):
+    __tablename__ = "Readers"
+    id = Column(Integer, primary_key=True, nullable=False)
+    id_vis = Column(Integer, ForeignKey("Visitors.id"), nullable=False)
+    id_book = Column(Integer, ForeignKey("Books.id"), nullable=False)
 
 Base.metadata.create_all(engine)
